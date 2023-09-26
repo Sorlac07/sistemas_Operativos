318ac972baf81d9cfa01de850edf7a2f0d29c17e
Jean Carlos <102634549+Sorlac09@users.noreply.github.com>
2023-09-26 18:37:06 -0500

    feat: mini crud

--- assets/scripts/productos.js
+++ assets/scripts/productos.js
@@ -20,7 +20,9 @@ fetch('http://172.191.145.152:3000/obtener-datos')
           </div>
           <button
           class='bg-[#8ab446] text-white py-4 px-10 mt-4 block mx-auto hover:bg-white hover:text-[#8ab446] rounded-2xl border-[#50F5431AC2D8] border-2 ease-in-out duration-300 border-[#8ab446]'
-          onClick={addItemsToCart}>Reservar</button>
+          onclick="buyProduct('${product.id}', ${product.price})">
+          Comprar
+      </button>
           </div>
         </div>
         `;
@@ -30,3 +32,47 @@ fetch('http://172.191.145.152:3000/obtener-datos')
   .catch((error) => {
     console.error(error);
   });
+
+/*
+ {
+   "usuario_id": 1,
+   "producto_id": 101,
+   "cantidad": 1,
+   "total": 50.00
+ }
+*/
+  //http://localhost:3000/agregar-compra
+
+
+  function buyProduct(productId, productPrice) {
+    fetch("http://172.191.145.152:3000/agregar-compra", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        usuario_id: 1,
+        producto_id: productId,
+        cantidad: 1,
+        total: productPrice,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("No se pudo obtener los datos");
+        }
+        return response.json();
+      }
+      )
+      .then((data) => {
+        console.log(data);
+        alert("Compra realizada con éxito");
+      }
+      )
+      .catch((error) => {
+        console.error(error);
+      }
+      );
+
+  }
+
--- assets/scripts/signup.js
+++ assets/scripts/signup.js
@@ -1,38 +1,42 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-import { auth } from "./firebase.js";
-import { ShowMessage } from "./showMessage.js";
-
 const signupForm = document.querySelector("#signup-form");
 
 signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+
+  const name = document.querySelector("#signup-name").value;
   const email = document.querySelector("#signup-email").value;
   const password = document.querySelector("#signup-password").value;
-  
-  try {
-    const userCredentials = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    ShowMessage("Bienvenido " + userCredentials.user.email);
 
-    // Espera un breve período de tiempo antes de redirigir
-    setTimeout(() => {
-      window.location.href = "hotels.html";
-    }, 3000); // Redirige después de 2 segundos (puedes ajustar este valor según tus necesidades)
-  } catch (error) {
-    if (
-      error.code === "auth/email-already-in-use" ||
-      error.code === "auth/network-request-failed"
-    ) {
-      ShowMessage("Ese correo electrónico ya está en uso", "error");
-    } else if (error.code === "auth/invalid-email") {
-      ShowMessage("Ese correo electrónico no es válido", "error");
-    } else if (error.code === "auth/weak-password") {
-      ShowMessage("La contraseña debe tener al menos 6 caracteres", "error");
-    } else if (error.code) {
-      ShowMessage(error.code, "error");
+  try {
+    // Realiza una solicitud HTTP POST al endpoint de tu backend
+    const response = await fetch("http://172.191.145.152:3000/agregar-usuario", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: name,
+        correo_electronico: email,
+        contrasena: password,
+      }),
+    });
+
+    // Verifica si la solicitud fue exitosa
+    if (response.ok) {
+      // El usuario se registró correctamente en el backend
+      const responseData = await response.json();
+      console.log(responseData);
+      // Redirige al usuario después de un breve período de tiempo
+      setTimeout(() => {
+        window.location.href = "productos.html";
+      }, 3000); // Redirige después de 3 segundos (ajusta según tus necesidades)
+    } else {
+      // Maneja errores en la respuesta (por ejemplo, usuario ya existe)
+      const responseData = await response.json();
     }
+  } catch (error) {
+    // Maneja otros errores
+    //console.error(error);
+    console.log("Error interno del servidor");
   }
 });
